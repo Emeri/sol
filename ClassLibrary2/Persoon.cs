74f7f448c9ad3fb35801ba7c24a9ba0c0a398d89
ep <ep@hiero.nl>
2015-07-10 12:09:01 +0200

    ja doen , en verder geen gezeur

---
+++ ClassLibrary2/Persoon.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ClassLibrary2
+{
+    public class Persoon
+    {
+        private DateTime _geboorteDatum;
+
+        public DateTime GeboorteDatum
+        {
+            get { return _geboorteDatum; }
+            set { _geboorteDatum = value; }
+        }
+
+        public int GeefLeeftijd()
+        {
+           
+            int leeftijd = (DateTime.Today.Year - this._geboorteDatum.Year) - 1;
+            if (DateTime.Today.Month > this._geboorteDatum.Month ||
+                    ((DateTime.Today.Month == this._geboorteDatum.Month) && (DateTime.Today.Day >= this._geboorteDatum.Day)))
+            {
+                leeftijd++;
+                // comment
+            }
+
+            return leeftijd;
+        }
+    }
+}
---
+++ UnitTestProject1/UnitTest1.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using ConsoleApplication1;
+using ClassLibrary2;
+
+
+
+namespace ClassLibrary2.Tests
+{
+    [TestClass()]
+    public class UnitTest1
+    {
+        [TestMethod()]
+        public void GeefLeeftijdTest()
+        {
+            Persoon p = new Persoon();
+
+            p.GeboorteDatum = new DateTime(1990, 07, 08);
+            Assert.AreEqual(24, p.GeefLeeftijd());
+        }
+    }
+}
+
+namespace UnitTestProject1
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        [TestMethod]
+        public void TestTelOp()
+        {
+            int result = Program.TelOp(2, 3);
+            Assert.AreEqual(result, 5);
+           
+        }
+    }
+}
